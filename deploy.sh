be731bf2b446bffcfabb87208cdce30105500e51
Todd Blackman <tdblackman@gmail.com>
2016-08-13 20:35:47 -0400

    Cleanup manual deployments

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 bundle exec middleman build
-aws s3 sync --region=us-west-2 --profile shawn build/ s3://www.coffeaconsulting.com/
+aws s3 sync --region=us-west-2 --profile shawn build/ s3://www.coffeaconsulting.com/ --delete
